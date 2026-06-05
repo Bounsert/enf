33dcc73d609dad483a4c5a69c640d3c7620d161d
Bounsert <jenagoshenko@gmail.com>
2026-06-05 19:08:24 +0300

    back to that

---
+++ cart/context_processors.py
@@ -0,0 +1,11 @@
+from .models import Cart
+
+def cart_processor(request):
+    if not request.session.session_key:
+        request.session.create()
+    cart, created = Cart.objects.get_or_create(
+        session_key = request.session.session_key
+    )
+
+    return('cart_total_item': cart.total_items,'cart_sumtotal': cart.sumtotal,)
+   
